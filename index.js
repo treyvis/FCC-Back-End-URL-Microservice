c8d7e3c01ac30da1264b158b43f7f5000a7ed1dc
Trey <treyvis@gmail.com>
2017-07-20 23:45:21 -0600

    regex on URL and ID, and pulling mongo ID

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const config = require('./config.json');
 const app = express();
 
-console.log(config);
+const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
+const idRegex = /^[\d\w]+$/g;
 
 const port = 4040;
 const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds121091.mlab.com:21091/url-shorten-microservice';
@@ -19,6 +21,22 @@ app.get('/', (req, res) => {
 				res.send(docs);
 			});
 	});
+});
+
+app.get('/:data', (req, res) => {
+	const data = req.params.data;
+	if (urlRegex.test(data)) {
+		res.send(data + ' is a valid URL');
+	} else if (idRegex.test(data)) {
+		mongo.connect(mongoURI, (err, db) => {
+			db.collection('urls').find(ObjectId(data)).next((err, doc) => {
+				console.log(doc);
+			});
+		});
+		res.send(data + ' is a valid ID');
+	} else {
+		res.send(data + ' is not a valid URL or ID');
+	}
 });
 
 app.listen(port, () => {
