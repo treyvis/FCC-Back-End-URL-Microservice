353b697fa20a95ed005a8f7a18d610fa0ea4490d
Trey <treyvis@gmail.com>
2017-07-21 00:53:34 -0600

    handle https in url

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
 const idRegex = /^[\d\w]+$/g;
+const httpRegex = /http(s)?:\/\//g;
 
 const port = 4040;
 const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds121091.mlab.com:21091/url-shorten-microservice';
@@ -23,17 +24,30 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.get('/:data', (req, res) => {
-	const data = req.params.data;
+app.get('/*?', (req, res) => {
+	const data = req.params[0];
+	console.log('Passed data:',data);
 	if (urlRegex.test(data)) {
+		let doc;
+		if (httpRegex.test(data)){
+			doc = {"url": data};
+		} else {
+			doc = {"url": "https://" + data};
+		}
+		mongo.connect(mongoURI, (err, db) => {
+			db.collection('urls').insert(doc, (err,doc) => {
+				if (err) throw err;
+				console.log('Generated URL:','https://fcc-urlmicroservice.glitch.me/' + doc.insertedIds[0]);
+			});
+		});
 		res.send(data + ' is a valid URL');
 	} else if (idRegex.test(data)) {
 		mongo.connect(mongoURI, (err, db) => {
 			db.collection('urls').find(ObjectId(data)).next((err, doc) => {
-				console.log(doc);
+				console.log('Redirect: ', doc.url);
+				res.redirect(doc.url);
 			});
 		});
-		res.send(data + ' is a valid ID');
 	} else {
 		res.send(data + ' is not a valid URL or ID');
 	}
