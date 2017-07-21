645b1b47ed320931883ddcc3dc03994db4964719
Trey <treyvis@gmail.com>
2017-07-20 20:44:38 -0600

    connecting and pulling from mongodb

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const config = require('./config.json');
 const app = express();
 
+console.log(config);
+
 const port = 4040;
+const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds121091.mlab.com:21091/url-shorten-microservice';
 
 app.get('/', (req, res) => {
-	
+	mongo.connect(mongoURI, (err, db) => {
+		if (err) throw err;
+		console.log('Connected to db');
+		db.collection('urls')
+			.find().toArray((err, docs) => {
+				if (err) throw err;
+				console.log(docs);
+				res.send(docs);
+			});
+	});
 });
 
 app.listen(port, () => {
